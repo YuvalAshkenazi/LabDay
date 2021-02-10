aaba2765617cccad3bb7de5f5f34e7a7a28ea872
yuval <yuvalash1@gmail.com>
2021-02-10 06:33:10 +0200

    Add navigation to all screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/ui/screens/AllTasks.dart';
+import 'package:flutter_app/ui/screens/EndShift.dart';
 import 'package:flutter_app/ui/screens/main_screen/main_screen.dart';
 import 'package:flutter_app/ui/screens/new_shift/new_shift.dart';
-import 'package:flutter_app/ui/screens/patients/patientPage.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_app/ui/screens/patients/MyPatients.dart';
 
-import 'bussiness_logic/models/patient.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +14,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Form Validation Demo';
-
-    //return NewShift();
-
-    return MainScreen();
-    /*return PatientPage(
-      patient: Patient('NRN 3', 'Mati Caspi',
-          'https://www.maariv.co.il/HttpHandlers/ShowImage.ashx?id=322703&w=500&h=380'),
-    );*/
+    
+    return MaterialApp(
+      title: 'Routes' ,
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => MainScreen(),
+        '/NewShift': (context) => NewShift(),
+        '/EndShift': (context) => EndShift(),
+        '/AllTasks': (context) => AllTasks(),
+        '/MyPatients': (context) => MyPatients(),
+      },
+    );
   }
 }
+
